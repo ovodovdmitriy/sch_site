d7b23956cc720c5d4bef4c13a5ea878ac2b23c20
ovodovdmitriy <ovodovdmitriy13@gmail.com>
2022-01-18 21:25:25 +0300

    Added view and template for detailed blog post as well as CSS for the site.

--- sch_site/sch_site/settings.py
+++ sch_site/sch_site/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r987mj%u&z8m&ipmii!qp=2r7hyr*(m=^=e=fm)wi5*p8zm66t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -109,9 +109,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
+'''STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "static"),
-]
+]'''
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
