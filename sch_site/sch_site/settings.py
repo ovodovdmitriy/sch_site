737207905550c53d3c01e30e163adfaa74a41600
ovodovdmitriy <92539680+ovodovdmitriy@users.noreply.github.com>
2022-01-19 19:39:44 +0300

    Update settings.py

--- sch_site/sch_site/settings.py
+++ sch_site/sch_site/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r987mj%u&z8m&ipmii!qp=2r7hyr*(m=^=e=fm)wi5*p8zm66t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ovodovdmitriy13.pythonanywhere.com]
 
 
 # Application definition
